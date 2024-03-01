394ffa05d2fbade6ea240ba598f8ed83a683cf01
Yeming Hu <yeminghuhym@gmail.com>
2024-03-01 10:22:43 -0500

    created navbar,hero,footer,properties,propertycard... pages

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
+
+const PropertyPage = () => {
+  const router = useRouter();
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+  const pathname = usePathname();
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {pathname}
+      </button>
+    </div>
+  );
+};
+export default PropertyPage;
