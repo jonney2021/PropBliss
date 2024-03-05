9136ca4be1565c9b85e67ea8f2330f5f3c0b5af1
Yeming Hu <yeminghuhym@gmail.com>
2024-03-04 22:04:18 -0500

    Profile & Manage Properties

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,9 @@ import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "PropBliss | Find The Perfect Rental",
@@ -17,6 +20,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
