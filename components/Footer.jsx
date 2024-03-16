cf9c13ae853b579ce555fe264f206e3585d60201
Yeming Hu <yeminghuhym@gmail.com>
2024-03-15 23:00:44 -0400

    Pagination, Featured, Lightbox

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
-            &copy; {currentYear} PropertyPulse. All rights reserved.
+            &copy; {currentYear} PropBliss. All rights reserved.
           </p>
         </div>
       </div>
